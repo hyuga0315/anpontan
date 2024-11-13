5f745e54e9ed9a342f11c8a8cb6f7a36645e2c71
hyuga0315 <hyugadq1730@gmail.com>
2024-11-13 14:12:02 +0900

    Update urls.py

--- anpontan/mychat/urls.py
+++ anpontan/mychat/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.startView, name='start'),
     #新規ユーザ登録画面
     path('createuser', views.createUser, name='createuser'),
+    # ユーザ登録結果作業画面
+    path('adduser', views.addUser, name='adduser'),
 ]
