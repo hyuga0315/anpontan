92118e0f8e146fc47172acfaae5fa494c7b8a1e6
sakaki-d <ririsu-702@docomo.ne.jp>
2024-11-27 14:20:03 +0900

    Update models.py

--- anpontan/mychat/models.py
+++ anpontan/mychat/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     name = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
     group = models.IntegerField(choices=[(1, "起業ユーザ"), (2, "就活ユーザ")], default=1)
-
+    islogin = models.BooleanField(default=False)
 
 class Room(models.Model):
     name = models.TextField(null=False, blank=False)
