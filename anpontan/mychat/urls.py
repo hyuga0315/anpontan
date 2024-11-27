ad77051595a24826d51e7937e3ed350a71b3fcdb
shusasaki0410 <syuu_0410@icloud.com>
2024-11-27 13:13:21 +0900

    Update urls.py

--- anpontan/mychat/urls.py
+++ anpontan/mychat/urls.py
@@ -4,10 +4,12 @@ from . import views
 app_name = 'mychat'
 
 urlpatterns = [
-    #ログイン認証画面
-    path('', views.startView, name='start'),
-    #新規ユーザ登録画面
-    path('createuser', views.createUser, name='createuser'),
-    # ユーザ登録結果作業画面
-    path('adduser', views.addUser, name='adduser'),
-]
+    path('',views.mainView,name='main'),
+    path('createuser',views.createUser,name='createuser'),
+    path('adduser',views.addUser,name='adduser'),
+    path('logout',views.logout,name='logout'),
+    path('room/<int:id>/',views.roomView,name='room'),
+    path('createroom',views.createRoom,name='createroom'),
+    path('addroom',views.addRoom,name='addroom'),
+    path('search',views.searchView,name='search'),
+] 
